11c2c1d2990b917fa3ec4c0a010e6028269b2c4d
Hiram Cruz <hiram0101@gmail.com>
2018-05-25 10:20:07 -0400

    fixed error in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,24 +2,24 @@
 import { API_BASE_URL } from '../config'
 
 export const FETCH_CHEESES_REQUEST = 'FETCH_CHEESES_REQUEST'
-export const fetchCheesesRequest = () => {
+export const fetchCheesesRequest = () => ({
   // eslint-disable-next-line
   type: FETCH_CHEESES_REQUEST
-}
+})
 
 export const FETCH_CHEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS'
-export const fetchCheesesSuccess = (cheeses) => {
+export const fetchCheesesSuccess = (cheeses) => ({
   // eslint-disable-next-line
   type: FETCH_CHEESES_SUCCESS,
   cheeses
-}
+})
 
 export const FETCH_CHEESES_ERROR = 'FETCH_CHEESES_ERROR'
-export const fetchCheesesError = (error) => {
+export const fetchCheesesError = (error) => ({
   // eslint-disable-next-line
   type: FETCH_CHEESES_ERROR,
   error
-}
+})
 
 export const fetchCheeses = () => dispatch => {
   dispatch(fetchCheesesRequest())
